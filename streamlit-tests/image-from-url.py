3f6d688c68e83ac87e416e7a454436d952571c28
Alexis Tuchinda <50222834+AlexisTuchinda@users.noreply.github.com>
2022-10-17 10:20:11 -0700

    Delete streamlit-tests directory
    this was just for drafting - unnecessary now

--- streamlit-tests/image-from-url.py
+++
@@ -1,7 +0,0 @@
-#testing to see if it is possible to display an image from url on streamlit (instead of having to download the image locally)
-
-import streamlit as st
-
-def display_images(images):
-    for url in images: 
-        st.markdown("![Image]("+url+")") #successful in displaying from url

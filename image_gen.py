cb828ac3bc9dc6eb55f8b1ea769314621c4ff04d
Taiga Nishida <tainish@nuevaschool.org>
2022-11-17 21:56:33 -0800

    Update image_gen.py

--- image_gen.py
+++ image_gen.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = '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'
+openai.api_key = '[insert api key (ofc i wont give you mine)]'
 
 def gen_images(prompt):
     response = openai.Image.create(

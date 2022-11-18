f6b09c018004daf9490c7305177a12966c200232
Taiga Nishida <tainish@nuevaschool.org>
2022-11-17 22:06:44 -0800

    Update image_gen.py

--- image_gen.py
+++ image_gen.py
@@ -5,8 +5,8 @@ openai.api_key = '[insert api key (ofc i wont give you mine)]'
 def gen_images(prompt):
     response = openai.Image.create(
         prompt=prompt,
-        n=1,
-        size="1024x1024"
+        n=1, # increase for more sample images
+        size="1024x1024" # increase for higher resolution
     )
     image_url = response['data'][0]['url']
 

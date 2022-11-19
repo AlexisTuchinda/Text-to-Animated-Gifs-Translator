902c4f3fa314c512aea3da5f45ee6d1d4c610082
Beckett <becleem@nuevaschool.org>
2022-11-18 16:02:02 -0800

    created an image carousel using an external react project

---
+++ CAROUSEL.md
@@ -0,0 +1,6 @@
+# Setting up the image carousel
+- cd into the `carousel` directory
+- run `npm install` to install the dependencies
+- run `npm run build` to build the project
+- cd back to the root directory
+- run `streamlit run app.py` to start the app
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True' #bypasses an error in local accessing. 
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"  # bypasses an error in local accessing.
 
 import streamlit as st
 import streamlit.components.v1 as components
@@ -13,55 +14,50 @@ from testing import current_tests
 def pipeline(image_file_buffer):
     image = Image.open(image_file_buffer)
 
-    #generate testing cases
-    #image.save("Testing/Unit-Tests/sample-5.jpg")
-    
-    #image to text
-    full_text=get_text(image) 
-   
-    #segment by sentence and use that to look for gifs
-    prompts = make_prompts (full_text)
+    # generate testing cases
+    # image.save("Testing/Unit-Tests/sample-5.jpg")
 
-    #Temporary image scrape & display
+    # image to text
+    full_text = get_text(image)
+
+    # segment by sentence and use that to look for gifs
+    prompts = make_prompts(full_text)
+
+    # Temporary image scrape & display
     urls = []
+    # SEE CAROUSSEL.md
     _carousel = components.declare_component("image_carousel", path="carousel/build")
 
     for prompt in prompts:
-        prompt.replace("\n","")
+        prompt.replace("\n", "")
         temp = load_images(1, prompt)
-        urls.append(temp[0])
-        
+        try:
+            urls.append(temp[0])
+        except:
+            urls.append(temp)
+
     if len(urls) == len(prompts):
-        _carousel(urls=urls, prompts=prompts, height=800) # needs a the urls and prompts, they need to be the same length, and the height of the carousel
+        _carousel(
+            urls=urls, prompts=prompts, height=850
+        )  # needs the urls and prompts, they need to be the same length, and the height of the carousel
     else:
         st.write("error")
 
-
-
-
-    #HOW TO DO IMAGE CAROUSEL OR IMAGE SLIDER TO CONTROL IMAGE DISPLAY 
-    #WITHOUT MAKING CODE RUN FOREVER OR HAVING WIDGETS BE RESET EVERY SINGLE TIME?
-    #formatting would be nice
+    # HOW TO DO IMAGE CAROUSEL OR IMAGE SLIDER TO CONTROL IMAGE DISPLAY
+    # WITHOUT MAKING CODE RUN FOREVER OR HAVING WIDGETS BE RESET EVERY SINGLE TIME?
+    # formatting would be nice
 
 
 def home():
     st.title("[software studio project title]")
-    st.write("Finds images from Google Search to help decipher instructions from manuals.")
+    st.write(
+        "Finds images from Google Search to help decipher instructions from manuals."
+    )
 
-    #camera input
+    # camera input
     image_file_buffer = st.camera_input("Take a photo")
     if image_file_buffer is not None:
         pipeline(image_file_buffer)
-
-    
-
-    
-
-
-
-
-        
-        
 
 
 home()
